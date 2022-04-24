11d68a02ea82447571035e88a35fcb4e8734e2e2
gaoda1 <104048691+gaoda1@users.noreply.github.com>
2022-04-24 22:33:29 +0800

    Add files via upload

---
+++ Project220424/Project220424/源.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+//1,库函数，2，自定义函数
+//
+//int Add(int x,int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int sum = Add(a, b);
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//int main()
+//{
+//	//strcpy-string copy-字符串拷贝
+//	//strlen-string length-字符串长度有关
+//	char arr1[] = "bit";
+//	char arr2[20] = "#####";
+//	strcpy(arr2,arr1);
+//	printf("%s\n", arr2);
+//	
+//	return 0;
+//}
+
+/*memset 
+  memory-内存（memset） set -内置*/
+
+
+in
