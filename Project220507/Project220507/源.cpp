f5506a8d503364e6dd16c770f8f231d772607ab5
gaoda1 <104048691+gaoda1@users.noreply.github.com>
2022-05-07 23:12:59 +0800

    Add files via upload

---
+++ Project220507/Project220507/源.cpp
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int a = 15;
+//	a = a ^ (1 << 2);
+//	/*0000000000000001011
+//	  0000000000000000100
+//	  0000000000000001111
+//	*/
+//KUIOO	
+//	printf("%d\n", a);//=15
+//	return 0;
+//}
+//int get_max(int x, int y)
+//{
+//  return x > y ? x : y;
+//}
+//
+//int main()
+//{
+//	//	int a = 10;
+//	//	printf("%d\n", ++a);//前置++ ;先++后使用
+//	/*int a = (int)3.14;
+//	printf("d\n", a);*/
+//	/*int i = 0, a = 1, b = 2, c = 3, d = 4;
+//	i = a++ && ++b && d++;
+//	printf("%d %d %d %d", a, b, c, d);*/
+//
+//	/*exp1 ? 真exp2 :真exp3*/
+//	/*int a = 0;
+//	int b = 9;
+//	int p = a > b ? a:b;
+//	printf("%d\n", p);*/
+//	int a = 20;
+//	int b = 40;
+//	int max = get_max(a, b);
+//	printf("%d\n", max);
+//
+//
+//	return 0;
+//}
+//struct stu//结构体类型-struct stu
+//{
+//	char name[20];
+//	int age;
+//	char id[20];
+//};
+//int main()
+//{
+//	int a = 10;
+//	struct stu s1 = { "tim",20,"2018900023"};
+//	struct stu *ps = &s1;
+//	printf("%s\n", (*ps).name);
+//	printf("%d\n", (*ps).age);
+//    return 0;
+//}
+
+//int main()
+//{
+//	char a = 3;
+//	char b = 250;//整形提升
+//	char c = a + b;
+//	printf("%d\n", c);
+//
+//	return 0;
+//
+//}
